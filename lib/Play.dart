cc2f19c83508dc34d59a0249766beb1b82f8c764
Higor Rocha <you@example.com>
2021-08-14 09:54:41 -0300

    Creating interface

--- lib/Play.dart
+++ lib/Play.dart
@@ -8,12 +8,27 @@ class Play extends StatefulWidget {
 }
 
 class _PlayState extends State<Play> {
+
+  void _showResult(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff000000),
+      backgroundColor: Color(0xff61bd86),
       body: Container(
-
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("images/logo.png"),
+            GestureDetector(
+              onTap: _showResult,
+              child: Image.asset("images/botao_jogar.png"),
+            )
+          ],
+        ),
       ),
     );
   }
